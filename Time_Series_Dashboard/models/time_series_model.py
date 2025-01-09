48f3cf7ef46dc95524a04d8ac1c9bd347688dee7
emrulk1 <emrulkais271@gmail.com>
2025-01-09 12:04:57 +0600

    Added gitignore to ignore venv folders

--- Time_Series_Dashboard/models/time_series_model.py
+++ Time_Series_Dashboard/models/time_series_model.py
@@ -10,6 +10,7 @@ class TimeSeriesAnalysis:
         self.model = None
         
     def create_sequences(self, data, seq_length):
+        """Create sequences for LSTM model"""
         sequences = []
         targets = []
         for i in range(len(data) - seq_length):
@@ -20,6 +21,7 @@ class TimeSeriesAnalysis:
         return np.array(sequences), np.array(targets)
     
     def build_model(self, seq_length):
+        """Build LSTM model"""
         model = Sequential([
             LSTM(50, activation='relu', input_shape=(seq_length, 1), return_sequences=True),
             LSTM(50, activation='relu'),
@@ -29,34 +31,59 @@ class TimeSeriesAnalysis:
         return model
     
     def train_model(self, data, seq_length=10, epochs=50):
+        """Train LSTM model with proper data scaling"""
+        # Scale the entire dataset first
         scaled_data = self.scaler.fit_transform(data.reshape(-1, 1))
+        
+        # Create sequences from scaled data
         X, y = self.create_sequences(scaled_data, seq_length)
+        
+        # Reshape X to (samples, time steps, features)
         X = X.reshape((X.shape[0], X.shape[1], 1))
         
+        # Build and train model
         self.model = self.build_model(seq_length)
         self.model.fit(X, y, epochs=epochs, verbose=0)
         
         return X, y
     
     def predict(self, X):
+        """Generate predictions"""
+        # Make predictions
         predictions = self.model.predict(X)
-        predictions = self.scaler.inverse_transform(predictions)
-        return predictions
+        # Inverse transform the predictions to get actual values
+        predictions_reshaped = predictions.reshape(-1, 1)
+        predictions_actual = self.scaler.inverse_transform(predictions_reshaped)
+        return predictions_actual
     
     def generate_forecast(self, data, lookback, forecast_days):
-        last_sequence = data[-lookback:].reshape(1, lookback, 1)
-        last_sequence = self.scaler.transform(last_sequence)
+        """Generate future forecasts"""
+        # Scale the input data
+        scaled_data = self.scaler.transform(data.reshape(-1, 1))
+        
+        # Prepare the last sequence
+        last_sequence = scaled_data[-lookback:]
+        last_sequence = last_sequence.reshape(1, lookback, 1)
         
         forecast = []
+        current_sequence = last_sequence.copy()
+        
+        # Generate predictions
         for _ in range(forecast_days):
-            next_pred = self.model.predict(last_sequence)
-            forecast.append(self.scaler.inverse_transform(next_pred)[0, 0])
-            last_sequence = np.roll(last_sequence, -1)
-            last_sequence[0, -1, 0] = next_pred[0, 0]
+            # Predict next value
+            next_pred = self.model.predict(current_sequence)
+            
+            # Add to forecast list (inverse transform to get actual value)
+            forecast.append(self.scaler.inverse_transform(next_pred.reshape(-1, 1))[0, 0])
+            
+            # Update sequence for next prediction
+            current_sequence = np.roll(current_sequence, -1, axis=1)
+            current_sequence[0, -1, 0] = next_pred[0]
         
         return forecast
     
     def calculate_metrics(self, actual, predicted):
+        """Calculate performance metrics"""
         mae = mean_absolute_error(actual, predicted)
         rmse = np.sqrt(mean_squared_error(actual, predicted))
         return mae, rmse
